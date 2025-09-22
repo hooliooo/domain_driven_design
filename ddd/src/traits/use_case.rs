0ef24cb76bffc23b8f906f45da2584e41ce8a73e
Julio Alorro <alorro3@gmail.com>
2025-09-22 23:27:07 +0200

    Add async trait

--- ddd/src/traits/use_case.rs
+++ ddd/src/traits/use_case.rs
@@ -1,5 +1,6 @@
 /// A UseCase is an object that organizes the business logic of core layer to fulfill
 /// a business requirement
+#[async_trait::async_trait]
 pub trait UseCase: Send + Sync {
     /// The input of the UseCase
     type Request;
@@ -8,5 +9,42 @@ pub trait UseCase: Send + Sync {
     type Response;
 
     /// Executes business logic within the core layer to fulfill a business requirement
-    fn handle(&self, request: Self::Request) -> impl Future<Output = Self::Response> + Send;
+    async fn handle(&self, request: Self::Request) -> Self::Response;
+}
+
+mod test {
+    use std::sync::Arc;
+
+    use crate::traits::use_case::UseCase;
+
+    struct TestUseCase;
+    struct TestRequest {
+        pub name: String,
+    }
+    struct TestResponse {
+        pub name: String,
+    }
+    struct TestError {
+        pub error: String,
+    }
+
+    #[async_trait::async_trait]
+    impl UseCase for TestUseCase {
+        type Request = TestRequest;
+        type Response = Result<TestResponse, TestError>;
+
+        async fn handle(&self, request: TestRequest) -> Result<TestResponse, TestError> {
+            Ok(TestResponse {
+                name: "test".to_string(),
+            })
+        }
+    }
+
+    struct TestState {
+        pub use_case: Arc<
+            dyn UseCase<Request = TestRequest, Response = Result<TestResponse, TestError>>
+                + Send
+                + Sync,
+        >,
+    }
 }
